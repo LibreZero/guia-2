d3dd6d08e003909eb9ceeaaa8d1eca2127f87d6e
LibreZero <73800147+LibreZero@users.noreply.github.com>
2022-06-17 21:01:36 -0400

    Add files via upload

---
+++ ejercicio guia 2.java
@@ -0,0 +1,16 @@
+import misClases.Animal;
+
+public class ejercicio
+{
+    public static void main(String[] args)
+    {
+
+        Animal miAnimal = new Animal("Falco");
+
+        miAnimal.setEdad(3);
+        
+        System.out.println("El nombre es: " + miAnimal.getNombre());
+
+        System.out.println(" y tiene " + miAnimal.getEdad() + " años");
+    }
+}
